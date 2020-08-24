86b977b39fecb183b62c0a7a00b25eba62bfc2a3
chicho86 <sergio.milici@gmail.com>
2020-08-24 10:34:46 -0300

    Add content to reproduce the bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  loaded$: Observable<boolean>;
+
+  constructor(private alertController: AlertController) {
+  }
+
+  ngOnInit() {
+    this.loaded$ = of(true).pipe(
+      // delay(1000)
+    );
+  }
+
+  async showMessage(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'header',
+      message: 'message',
+      buttons: [{text: 'Ok'}]
+    });
+    return await alert.present();
+  }
 
 }
